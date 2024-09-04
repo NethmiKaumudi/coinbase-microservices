dc3d0cffba2da22df06931ac66b7dd6c56847760
Nethmi Kaumudi <118107008+NethmiKaumudi@users.noreply.github.com>
2024-09-05 02:00:03 +0530

    update

--- wallet-service/app/app.py
+++ wallet-service/app/app.py
@@ -1,13 +1,3 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return jsonify(message="Wallet Service is running")
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5002)
 from flask import Flask
 from flask_restful import Api
 from routes.wallet_routes import HealthCheck, Wallet
